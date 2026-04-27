7341b58aac5f874560155b751d37333e302b0ea8
Lucas Carvalho <lucas.carvalho180807@gmail.com>
2026-04-27 08:40:42 -0300

    feat: adiciona estrutura html do compontente tela de login

---
+++ src/modules/login.js
@@ -0,0 +1,24 @@
+export function CreateLoginScreen() {
+    const section = document.createElement("section");
+    section.classList.add("login-section");
+
+    section.innerHTML = `
+    <div class="login-card">
+      <h2>Acesso - Aptus Defensio</h2>
+      <form id="login-form">
+        <div class="input-group">
+          <label for="email">E-mail ou Matrícula</label>
+          <input type="text" id="email" required placeholder="Insira seus dados">
+        </div>
+        <div class="input-group">
+          <label for="password">Senha</label>
+          <input type="password" id="password" required placeholder="Insira sua senha">
+        </div>
+        <button type="submit" class="btn-primary">Entrar</button>
+      </form>
+    </div>
+  `;
+
+  return section;
+    
+}
