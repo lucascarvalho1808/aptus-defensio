c8f03c8edb295ef0393a633a4f7e9f72d71296d8
Jose Carlos <jc.abreu.jr1001@gmail.com>
2026-06-30 21:41:24 -0300

    chore(database): adiciona políticas RLS para propostas

--- src/database/policies.sql
+++ src/database/policies.sql
@@ -12,23 +12,67 @@ alter table membros_banca enable row level security;
 
 alter table documentos enable row level security;
 
-create policy "allow all"
+--------------------------------------------------------
+-- USERS
+--------------------------------------------------------
+
+create policy "Usuários autenticados podem visualizar usuários"
 on users
-for all
-using (true)
-with check (true);
+for select
+to authenticated
+using (true);
 
-create policy "allow all temas"
+create policy "Usuário pode atualizar apenas o próprio registro"
+on users
+for update
+to authenticated
+using (
+    auth.uid() = id
+)
+with check (
+    auth.uid() = id
+);
+
+--------------------------------------------------------
+-- TEMAS
+--------------------------------------------------------
+
+create policy "Usuários autenticados podem visualizar temas"
 on temas
-for all
-using (true)
-with check (true);
+for select
+to authenticated
+using (true);
 
-create policy "allow authenticated students to create orientacoes"
-on orientacoes
+create policy "Professor pode criar temas"
+on temas
 for insert
 to authenticated
-with check ((select auth.uid()) = aluno_id);
+with check (
+    auth.uid() = created_by
+);
+
+create policy "Professor pode atualizar seus temas"
+on temas
+for update
+to authenticated
+using (
+    auth.uid() = created_by
+)
+with check (
+    auth.uid() = created_by
+);
+
+create policy "Professor pode excluir seus temas"
+on temas
+for delete
+to authenticated
+using (
+    auth.uid() = created_by
+);
+
+--------------------------------------------------------
+-- PROPOSTAS
+--------------------------------------------------------
 
 create policy "Aluno pode criar a propria proposta"
 on propostas
@@ -46,7 +90,30 @@ using (
     auth.uid() = aluno_id
 );
 
-create policy "professor pode visualizar suas orientacoes"
+create policy "Aluno pode atualizar a propria proposta"
+on public.propostas
+for update
+to authenticated
+using (
+    auth.uid() = aluno_id
+)
+with check (
+    auth.uid() = aluno_id
+);
+
+--------------------------------------------------------
+-- ORIENTAÇÕES
+--------------------------------------------------------
+
+create policy "Aluno pode criar solicitação de orientação"
+on orientacoes
+for insert
+to authenticated
+with check (
+    auth.uid() = aluno_id
+);
+
+create policy "Professor pode visualizar suas orientações"
 on orientacoes
 for select
 to authenticated
@@ -54,7 +121,7 @@ using (
     auth.uid() = professor_id
 );
 
-create policy "professor pode atualizar suas orientacoes"
+create policy "Professor pode atualizar suas orientações"
 on orientacoes
 for update
 to authenticated
